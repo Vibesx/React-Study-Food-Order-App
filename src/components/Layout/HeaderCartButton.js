46ff0d54d0a9141232d8d500809c29f4ee95c216
vibesx <lucian.paslariu@gmail.com>
2022-10-19 13:18:00 +0300

    initial commit

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,17 @@
+import classes from "./HeaderCartButton.module.css";
+
+import CartIcon from "../Cart/CartIcon";
+
+const HeaderCartButton = (props) => {
+	return (
+		<button>
+			<span>
+				<CartIcon></CartIcon>
+			</span>
+			<span></span>
+			<span></span>
+		</button>
+	);
+};
+
+export default HeaderCartButton;
